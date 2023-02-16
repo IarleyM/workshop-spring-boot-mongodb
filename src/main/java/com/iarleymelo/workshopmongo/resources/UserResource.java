fc357c0f4c14524a7ff05b779e2ef10cf25cf929
IarleyM <iarleym771@gmail.com>
2023-02-16 18:48:09 -0300

    EndPoint para retornar os posts de um usuario

--- src/main/java/com/iarleymelo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/iarleymelo/workshopmongo/resources/UserResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.iarleymelo.workshopmongo.domain.Post;
 import com.iarleymelo.workshopmongo.domain.User;
 import com.iarleymelo.workshopmongo.dto.UserDTO;
 import com.iarleymelo.workshopmongo.services.UserService;
@@ -59,5 +60,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.Update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value ="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
